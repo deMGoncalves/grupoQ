c55007eeed5c361624baecf92df9450329f96749
deMGoncalves <cleber.engineer@hotmail.com>
2023-05-05 09:58:59 -0300

    refactor(home): neste momento o build da home eh muito pequeno

--- site/home/src/index.jsx
+++ site/home/src/index.jsx
@@ -1,7 +1,8 @@
 import h, { render } from '@grupoq/h'
+import Home from './home'
 import router from '@grupoq/router'
 
-router('/', async function home () {
-  const { default: Home } = await import('./home' /* webpackChunkName: "home" */)
+router('/', function home () {
+  // const { default: Home } = await import('./home' /* webpackChunkName: "home" */)
   render(document.body, <Home />)
 })

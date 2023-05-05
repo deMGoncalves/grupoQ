19f9ddcb3557a6a5e0f16a2177cc6443ac85bfc3
deMGoncalves <cleber.engineer@hotmail.com>
2023-05-05 10:44:23 -0300

    chore(card): inclusao do titulo do produto

--- component/card/src/component.jsx
+++ component/card/src/component.jsx
@@ -1,11 +1,13 @@
 import h from '@grupoq/h'
 import Img from '@grupoq/img'
 import style from './style'
+import text from '@grupoq/text'
 
 function component (card) {
   return (
     <section className={style.card}>
-      <Img className={style.card__img} src={card.image} />
+      <Img className={style.card__image} src={card.image} />
+      <text.Strong master dark xxs medium>{card.title}</text.Strong>
     </section>
   )
 }
--- component/card/src/style.js
+++ component/card/src/style.js
@@ -6,10 +6,13 @@ export default styled`
     border-radius: var(--border-radius-sm);
     content-visibility: auto;
     cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing_inset-xs);
     padding: var(--spacing_inset-xs);
   }
 
-  .card__img {
+  .card__image {
     aspect-ratio: 3 / 4;
     object-fit: contain;
     width: 100%;

d4dfc329a11f4d63590181f5166a0628dac93041
deMGoncalves <cleber.engineer@hotmail.com>
2023-05-06 10:10:08 -0300

    chore(product): inclusao das calhas para os elementos

--- site/product/src/style.jsx
+++ site/product/src/style.jsx
@@ -4,4 +4,10 @@ export default styled`
   .product__main {
     padding: var(--spacing_inset-xs) 0;
   }
+
+  .product__main div {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing_inset-xs);
+  }
 `

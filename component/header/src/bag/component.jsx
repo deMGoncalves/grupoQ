ca6fbf924f525b8e2e901705ce76131aa27ed1f3
deMGoncalves <cleber.engineer@hotmail.com>
2023-05-06 19:29:08 -0300

    refactor(header): nao exibit o badge quando nao tiver itens na sacola

--- component/header/src/bag/component.jsx
+++ component/header/src/bag/component.jsx
@@ -2,13 +2,16 @@ import Badge from '@grupoq/badge'
 import h from '@grupoq/h'
 import icon from './icon.png'
 import Img from '@grupoq/img'
+import Show from '@grupoq/show'
 import style from './style'
 
 function component (bag) {
   return (
     <button className={style.bag} onClick={() => bag.open()}>
       <Img className={style.bag__icon} src={icon} />
-      <Badge className={style.bag__count}>{bag.count}</Badge>
+      <Show when={bag.count}>
+        <Badge className={style.bag__count}>{bag.count}</Badge>
+      </Show>
     </button>
   )
 }

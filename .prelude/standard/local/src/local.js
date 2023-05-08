04f17c197e37ccd2d764aa3be7dd008bb2960fba
deMGoncalves <cleber.engineer@hotmail.com>
2023-05-08 08:47:25 -0300

    feat(local): pacote para manipular o localstorage

---
+++ .prelude/standard/local/src/local.js
@@ -0,0 +1,12 @@
+const local = new Proxy({}, {
+  get (_target, key) {
+    const value = localStorage.getItem(key)
+    return value
+      ? JSON.parse(value)
+      : undefined
+  },
+
+  set (_target, key, value) {
+    return localStorage.setItem(key, JSON.stringify(value)), true
+  }
+})
